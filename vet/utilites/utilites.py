31702e3a72b5584e3ef34f14601b78be0a71f06e
Mihail589 <107617176+Mihail589@users.noreply.github.com>
2025-12-05 17:18:42 +0300

    Delete vet directory

--- vet/utilites/utilites.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np  
-
-def bytes_to_cvimage(image_bytes):
-    # Преобразование байтов в numpy массив
-    nparr = np.frombuffer(bytes.fromhex(image_bytes), np.uint8)
-    # Декодирование изображения
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
